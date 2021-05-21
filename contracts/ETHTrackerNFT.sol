a4651bc4efc4481cd6cf0b1d51fae8982865ab07
Steven <istvn@icloud.com>
2021-05-21 17:18:11 +0100

    Pre-testing

--- contracts/ETHTrackerNFT.sol
+++ contracts/ETHTrackerNFT.sol
@@ -8,13 +8,13 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract ETHTrackerNFT is ERC721 {
 
     enum Status {
-        Up1,
-        Up2,
-        Up5,
-        Down1,
-        Down2,
-        Down5,
-        twentyThousand
+        Up1,           // 0
+        Up2,           // 1
+        Up5,           // 2
+        Down1,         // 3
+        Down2,         // 4
+        Down5,         // 5
+        twentyThousand // 6
     }
 
     AggregatorV3Interface private priceFeed;
@@ -25,15 +25,16 @@ contract ETHTrackerNFT is ERC721 {
     int public latestPrice;
     uint public latestDateChecked;
 
+    int16 public trend; // consecutive days price appreciated/depreciated (unchanged if price remains same)
 
-    constructor() ERC721("ETHTrackerNFT", "ETN") {
+
+    constructor(string memory baseURI, address priceFeedOracle) ERC721("ETHTrackerNFT", "ETN") {
+        _base = baseURI;
+        priceFeed = AggregatorV3Interface(priceFeedOracle);
         _mint(msg.sender, 0);
-
-        priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e); // Rinkeby
-        _base = "https://base.com/";
-
-        //TODO run _getThePrice and set that as latestPrice
-            //TODO set block.timestamp as latestDateChecked... NO, FIGURE OUT THE DATE's timestamp. do date stuff
+        (int price, uint timestamp) = _getLatestPrice();
+        latestPrice = price;
+        latestDateChecked = _getDateTimestamp(timestamp);
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
@@ -41,21 +42,82 @@ contract ETHTrackerNFT is ERC721 {
         return string(abi.encodePacked(_base, _status));
     }
 
-    function _setTokenURI(Status status) private {
-        _status = status;
-    }
-
-    function _getThePrice() private view returns (int) {
+    function _getLatestPrice() private view returns (int, uint) {
         (
             uint80 roundID,
             int price,
             uint startedAt,
-            uint timeStamp,
+            uint timestamp,
             uint80 answeredInRound
         ) = priceFeed.latestRoundData();
-        return price;
+        return (price, timestamp);
     }
 
+    function _getDateTimestamp(uint timestamp) private pure returns(uint) {
+        uint32 secondsInDay = 86_400;
+
+        uint currentDayTimestamp = timestamp % secondsInDay;
+        uint currentDateTimestamp = timestamp - currentDayTimestamp;
+        return currentDateTimestamp;
+    }
+
+    function updatePrice() public {
+        (
+            int price,
+            uint timestamp // the time price feed last updated (not the time oracle called)
+        ) = _getLatestPrice();
+        uint date = _getDateTimestamp(timestamp);
+
+        require(date != latestDateChecked, "Latest price from price feed has already been set for the day");
+
+        int prevPrice = latestPrice;
+        uint prevDate = latestDateChecked;
+        uint32 secondsInDay = 86_400;
+
+        if (price - prevPrice > 0) { // price appreciated
+
+            if (prevDate == date - secondsInDay) { // previous check was previous day
+                trend > 0 ? trend++ : trend = 1;
+            } else {
+                trend = 1;
+            }
+
+            if (price > 20_000) {
+                _status = Status.twentyThousand;
+            } else if (trend >= 5) {
+                _status = Status.Up5;
+            } else if (trend >= 2) {
+                _status = Status.Up2;
+            } else if (trend == 1) {
+                _status = Status.Up1;
+            }
+
+        } else if (price - prevPrice < 0) { // price depreciated
+
+            if (prevDate == date - secondsInDay) { // previous check was previous day
+                trend < 0 ? trend-- : trend = -1;
+            } else {
+                trend = -1;
+            }
+
+            if (price > 20_000) {
+                _status = Status.twentyThousand;
+            } else if (trend <= -5) {
+                _status = Status.Down5;
+            } else if (trend <= 2) {
+                _status = Status.Down2;
+            } else if (trend <= 1) {
+                _status = Status.Down1;
+            }
+
+        } else { // price remained same
+            // trend and _status remain unchanged
+            return;
+        }
+
+        latestPrice = price;
+        latestDateChecked = date;
+    }
 
 
 }
